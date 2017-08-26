8299b36f44913046044a0add2f2e3e7055ac9452
Marc <marc@solidearthvr.com>
2017-08-26 02:14:31 -0700

    added destroy colliding enemy script

---
+++ Assets/_Project/Scripts/DestroyCollidingEnemy.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyCollidingObject : MonoBehaviour {
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            EnemyManager.numEnemies--;
+        }
+    }
+}
