0e03f7652cc743e87fabd85974b497bc60daa56c
MkfShard <hartmaniac@gmail.com>
2017-08-26 08:53:17 -0700

    So I can work on them elsewhere
    Lab is closed for open house, so moving files

--- Assets/_Project/Joseph/Scripts/FPSCamera.cs
+++ Assets/_Project/Joseph/Scripts/FPSCamera.cs
@@ -8,25 +8,34 @@ public class FPSCamera : MonoBehaviour
     public float horSensitivity = 10.0f;
     public float verSensitivity = 10.0f;
 
-    public float upperLimit = 80.0f;
-    public float lowerLimit = 80.0f;
+    public float viewLimit = 80.0f;
 
     private float horInput;
     private float verInput;
 
+    private float rotateY = 0f;
+
+
 
     // Use this for initialization
-    void Start ()
+    void Update ()
     {
-		
-	}
+        horInput = Input.GetAxis("Mouse X");
+        verInput = Input.GetAxis("Mouse Y");
+
+        rotateY += transform.localEulerAngles.y + verInput * verSensitivity;
+
+        float rotateX = transform.localEulerAngles.x + horInput * horSensitivity;
+        //rotateY = Mathf.Clamp(rotateY, -viewLimit, viewLimit);
+
+        transform.localEulerAngles = new Vector3(-rotateY, rotateX, 0);
+
+
+    }
 	
 	// Update is called once per frame
+	void LateUpdate ()
     {
-        horInput = Input.GetAxis("Horizontal");
-        verInput = Input.GetAxis("Vertical");
-
-
 
 
     }
