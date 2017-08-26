edec69c12f8cd192e3df568132ed0a86aeda1d7f
MkfShard <hartmaniac@gmail.com>
2017-08-26 00:48:53 -0700

    Joseph Folder
    added Health.cs, incomplete FPSCamera

---
+++ Assets/_Project/Joseph/Scripts/FPSCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCamera : MonoBehaviour
+{
+
+    public float horSensitivity = 10.0f;
+    public float verSensitivity = 10.0f;
+
+    public float upperLimit = 80.0f;
+    public float lowerLimit = 80.0f;
+
+    private float horInput;
+    private float verInput;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+    {
+        horInput = Input.GetAxis("Horizontal");
+        verInput = Input.GetAxis("Vertical");
+
+
+
+
+    }
+}
