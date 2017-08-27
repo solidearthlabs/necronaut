5b0066c3a7bd3ccc4000138f4a3803555bffd04a
Marc <marc@solidearthvr.com>
2017-08-26 19:26:04 -0700

    added enemy sounds and random clip player

---
+++ Assets/_Project/Scripts/EnemyAttack.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+
+
+    GameObject player;
+    Health playerHealth;
+    Health enemyHealth;
+    bool playerInRange;
+    float timer;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player");
+        playerHealth = player.GetComponent <Health> ();
+        enemyHealth = GetComponent<Health>();
+    }
+
+
+    void OnTriggerEnter (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit (Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            playerInRange = false;
+        }
+    }
+
+
+    void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth._currentHealth > 0)
+        {
+            Attack ();
+        }
+
+        if(playerHealth._currentHealth <= 0)
+        {
+            //StartCoroutine(Die(),0);
+        }
+    }
+
+
+    void Attack ()
+    {
+        timer = 0f;
+
+        if(playerHealth._currentHealth > 0)
+        {
+            playerHealth.DamageHealth(attackDamage);
+        }
+    }
+}
