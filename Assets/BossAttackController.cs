cf09182d2ea443dba25027ef5c2a365a99d0fa65
Marc <marc@solidearthvr.com>
2017-08-27 09:09:14 -0700

    misc shader changes

---
+++ Assets/BossAttackController.cs
@@ -0,0 +1,141 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackController : MonoBehaviour {
+    public GameObject bullet;
+    public float timeBetweenAttacks = 5f;
+
+    //apply to gun
+    public enum bulletStyle { fire, blob, swarm, lightning, shockwave, bomb }
+    public bulletStyle bSty = bulletStyle.fire;
+    public int bulletNumber = 1;
+    public float inaccuracy = 0;
+    public int heat = 30;
+    public int cooldown = 0;
+
+    public int chargePercent = 0;
+    public int windupReq = 60;
+
+    //apply to bullet iself
+    public float size = 1; //currently does nothing
+    public float fireForce = 1000;
+    public float fireSpeed = 0.2f;
+
+    private Quaternion aimDirection;
+    private GameObject player;
+    private GameObject[] shots;
+
+	// Use this for initialization
+	void Start () {
+        if (bullet == null)
+            Debug.LogError("bullet is not defined!!");
+
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player == null)
+            Debug.LogError("couldn't find player!!");
+
+        StartCoroutine(Attack());
+    }
+
+    IEnumerator Attack()
+    {
+        while (true)
+        {
+            // Aim bullet in player's direction.
+            aimDirection = Quaternion.LookRotation(player.transform.position);
+            fireBullet(aimDirection);
+        }
+    }
+
+    void fireBullet(Quaternion aimDir)
+    {
+        Instantiate(bullet);
+    }
+        // Update is called once per frame
+    void Update()
+    {
+            // Move the projectile forward towards the player's last known direction;
+            transform.position += transform.forward * fireSpeed * Time.deltaTime;
+    }
+    //playerDir = transform.position
+
+
+    public void attackChange(bulletStyle attack)
+    {
+
+        bSty = attack;
+        switch (attack) //Update gun stats here
+        {
+            case bulletStyle.fire:
+                {
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 20;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 1000;
+                    break;
+                }
+                // TODO: need to implement blob
+            case bulletStyle.blob:
+                {
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 100;
+                    windupReq = 0;
+                    size = 3;
+                    fireForce = 1200;
+                    break;
+                }
+                // TODO: Need to implement swarm
+            case bulletStyle.swarm:
+                {
+                    bulletNumber = 3;
+                    inaccuracy = .5f;
+                    heat = 0;
+                    windupReq = 0;
+                    size = 0.5f;
+                    fireForce = 200;
+                    break;
+                }
+                // TODO: Need to implement lightning
+            case bulletStyle.lightning:
+                {
+                    bulletNumber = 10;
+                    inaccuracy = 0.1f;
+                    heat = 60;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 800;
+                    break;
+                }
+                // TODO: Need to implement shockwave
+            case bulletStyle.shockwave:
+                {
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 0;
+                    windupReq = 20;
+                    size = 0.5f;
+                    fireForce = 2500;
+                    break;
+                }
+            case bulletStyle.bomb:
+                {
+                    bulletNumber = 6;
+                    inaccuracy = 0.2f;
+                    heat = 20;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 1000;
+                    break;
+                }
+
+
+        }
+        //shot = shots[(int)bSty];
+    }
+
+
+}
---
+++ Assets/_Project/Shaders/FractalProto.shader
@@ -0,0 +1,76 @@
+﻿// Upgrade NOTE: replaced 'PositionFog()' with transforming position into clip space.
+// Upgrade NOTE: replaced 'V2F_POS_FOG' with 'float4 pos : SV_POSITION'
+
+/
+// Shader: "FX/FractalProto"
+// Version: v1.0
+// Written by: Thomas Phillips
+//
+// Anyone is free to use this shader for non-commercial or commercial projects.
+//
+// Description:
+// Prototype fractal effect.
+// I was playing with the Mandelbrot set, but wasn't that happy
+// because it is essentially a static structure.
+// This was an attempt to breathe a litte life into the set.
+// I don't see an immediate application for this shader,
+// but I hope it will inspire.
+//
+
+Shader "FX/FractalProto" {
+
+	Properties{
+		_Color("Color Tint", Color) = (1,1,1,1)
+	}
+
+		SubShader{
+
+		ZWrite Off
+		Tags{ "Queue" = "Transparent" }
+		Blend One One
+
+		Pass{
+
+		CGPROGRAM
+#pragma vertex vert
+#pragma fragment frag
+#pragma fragmentoption ARB_fog_exp2
+#include "UnityCG.cginc"
+
+		float4 _Color;
+	float _Rate;
+
+	struct v2f {
+		float4 pos : SV_POSITION;
+		float4 texcoord : TEXCOORD0;
+	};
+
+	v2f vert(appdata_base v)
+	{
+		v2f o;
+		o.pos = UnityObjectToClipPos (v.vertex);
+		o.texcoord = v.texcoord - 0.5;
+		return o;
+	}
+
+	half4 frag(v2f i) : COLOR
+	{
+		float3 color;
+	float m;
+	float2 c = i.texcoord.xy;
+	float2 z = float2(_SinTime[1],_CosTime[1]);
+	int j;
+	for (j = 0; j< 7; j++) {
+		z = float2((z.x * z.x - z.y * z.y), (z.x * z.y + z.x * z.y)) + c;
+	} // for j
+	m = tan(pow(1 / length(z)*.5, .8));
+
+	color = float3(m*_Color.r, m*_Color.g, m*_Color.b);
+	return half4(color, 1);
+	}
+		ENDCG
+
+	}
+	}
+		Fallback "Transparent/Diffuse"
+}
