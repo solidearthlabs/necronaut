c6cc1db59c04683ec779ddb4cfe79cf292dbea3f
MkfShard <hartmaniac@gmail.com>
2017-08-26 17:55:04 -0700

    Latest Joseph
    Skybox, more shotmaker

--- Assets/_Project/Joseph/Scripts/FPSFire.cs
+++ Assets/_Project/Joseph/Scripts/FPSFire.cs
@@ -11,15 +11,12 @@ public class FPSFire : MonoBehaviour
     //apply to gun
     public enum bulletStyle { normal=0, fire=1, laser=2, bomb=3}
     public bulletStyle bSty = bulletStyle.normal;
-    public enum shootStyle { normal=0, charge=1, rapid=2}
-    public shootStyle sSty = shootStyle.normal;
     public int bulletNumber = 1;
     public float inaccuracy = 0;
     public int heat = 30;
     public int cooldown = 0;
 
     public int chargePercent = 0;
-
     public int windupReq = 60;
 
 
@@ -48,89 +45,36 @@ public class FPSFire : MonoBehaviour
             cooldown--;
         }
 
-        switch (sSty)
+        if (Input.GetMouseButton(0) && cooldown == 0)
         {
-            case (shootStyle.normal):
+            if (chargePercent >= windupReq)
             {
-                    if (Input.GetMouseButtonDown(0) && cooldown == 0)
-                    {
-                        cooldown = heat;
-
-
-
-
-                        for (int i = 0; i < bulletNumber; i++)
-                        {
-                            Vector3 inaccurate = Random.insideUnitSphere * inaccuracy;
-                            Quaternion inacc = Quaternion.Euler(inaccurate);
-                            GameObject bullet = Instantiate(shot, transform.position, transform.rotation * inacc) as GameObject;
-                            bullet.GetComponent<Rigidbody>().AddForce((transform.forward + inaccurate) * fireForce);
-                        }
-
-
-                    }
-                    break;
-            }
-            case (shootStyle.charge):
-            {
-                    if (Input.GetMouseButton(0) && cooldown == 0 && chargePercent < 100)
-                    {
-                        chargePercent++;
-                    }
-                    else if (Input.GetMouseButtonUp(0))
-                    { 
-                       
-
-
-                        //chargePercent powers up the shot
-
-
-
-                        for (int i = 0; i < bulletNumber; i++)
-                            {
-                                Vector3 inaccurate = Random.insideUnitSphere * inaccuracy;
-                                Quaternion inacc = Quaternion.Euler(inaccurate);
-                                GameObject bullet = Instantiate(shot, transform.position, transform.rotation * inacc) as GameObject;
-                                bullet.GetComponent<Rigidbody>().AddForce((transform.forward + inaccurate) * fireForce);
-                            }
-
-                        chargePercent = 0;
-                    }
-                    break;
-            }
-            case (shootStyle.rapid):
-            {
-                    if (Input.GetMouseButton(0) && cooldown == 0)
-                    {
-                        if (chargePercent >= windupReq)
-                        {
-                            cooldown = heat;
+                cooldown = heat;
                             
 
 
 
 
-                            for (int i = 0; i < bulletNumber; i++)
-                            {
-                                Vector3 inaccurate = Random.insideUnitSphere * inaccuracy;
-                                Quaternion inacc = Quaternion.Euler(inaccurate);
-                                GameObject bullet = Instantiate(shot, transform.position, transform.rotation * inacc) as GameObject;
-                                bullet.GetComponent<Rigidbody>().AddForce((transform.forward + inaccurate) * fireForce);
-                            }
-                        }
-                        else
-                        {
-                            chargePercent++;
-                        }
-
-
-                    }
-                    else if (chargePercent > 0)
-                    {
-                        chargePercent--;
-                    }
-                    break;
+                for (int i = 0; i < bulletNumber; i++)
+                {
+                    Vector3 inaccurate = Random.insideUnitSphere * inaccuracy;
+                    Quaternion inacc = Quaternion.Euler(inaccurate);
+                    GameObject bullet = Instantiate(shot, transform.position, transform.rotation * inacc) as GameObject;
+                    bullet.GetComponent<Rigidbody>().AddForce((transform.forward + inaccurate) * fireForce);
+                }
             }
+            else
+            {
+                chargePercent++;
+            }
+
+
+        }
+        else if (chargePercent > 0)
+        {
+            chargePercent--;
+        }
+            
 
 
 
@@ -142,10 +86,3 @@ public class FPSFire : MonoBehaviour
 
 
     }
-
-
-
-
-
-
-}
