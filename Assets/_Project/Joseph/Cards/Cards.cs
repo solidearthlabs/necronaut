4c2110d73dd67fa61ecdd0ed6b92058f3fa3b56a
MkfShard <hartmaniac@gmail.com>
2017-08-26 21:31:13 -0700

    Shotmaker Nearly Finished

--- Assets/_Project/Joseph/Cards/Cards.cs
+++ Assets/_Project/Joseph/Cards/Cards.cs
@@ -1,18 +1,99 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 using UnityEngine;
 
-public class Cards : MonoBehaviour {
+public class Cards : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start ()
+    public FPSFire gun;
+    public RawImage currentCard;
+
+
+    enum cards
     {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
+        normal = 0,
+        fireball = 1,
+        flamethrower = 2,
+        shotgun = 3,
+        machinegun = 4,
+        autoshot = 5,
+        bomb = 6,
+        laser = 7,
+        final = 8
+    }
+
+    public RawImage[] handImages;
+    public int[] hand;
+    public Texture[] CardImages;
+    public int selectedCard;
+    public RectTransform selectionPanel;
+
+
+
+    // Use this for initialization
+    void Start()
     {
-		
-	}
+        selectedCard = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float wheelInput = Input.GetAxis("Mouse ScrollWheel");
+
+        
+        if (wheelInput != 0)
+        {
+            selectedCard += (int)Mathf.Sign(wheelInput);
+            if (selectedCard > 2)
+            {
+                selectedCard = 0;
+            }
+            else if (selectedCard < 0)
+            {
+                selectedCard = 2;
+            }
+
+            RectTransform selectHand = handImages[selectedCard].GetComponent<RectTransform>();
+
+            selectionPanel.position = selectHand.position;
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            LoadCard((cards)hand[selectedCard]);
+        }
+
+
+
+
+    }
+
+    void LoadCard(cards card) //load a card into the Gun
+    {
+        currentCard.texture = ChooseCard((int)card);
+        DrawCard(selectedCard);
+        gun.GunChange((int)card);
+    }
+
+    int DrawCard (int goneCard) //grab random card to replace loaded one
+    {
+
+        
+        int newCard = Random.Range(0, 9);
+        hand[goneCard] = newCard;
+        handImages[goneCard].texture = ChooseCard(newCard);
+
+        return newCard;
+    }
+
+    public Texture ChooseCard (int chosen) //return a specific card
+    {
+        return CardImages[chosen];
+    }
+
+
+
 }
--- Assets/_Project/Joseph/Scripts/FPSFire.cs
+++ Assets/_Project/Joseph/Scripts/FPSFire.cs
@@ -23,18 +23,17 @@ public class FPSFire : MonoBehaviour
 
     //apply to bullet iself
 
-    public int size = 1;
-    public int shotSpeed = 1;
+    public float size = 1; //currently does nothing
     public float fireForce = 1000;
 
     //
 
-
+        //ADD LASER
 
     // Use this for initialization
     void Start ()
     {
-        shot = shots[(int)bSty];
+        GunChange(0);
     }
 	
 	// Update is called once per frame
@@ -78,11 +77,122 @@ public class FPSFire : MonoBehaviour
 
 
 
-        }
-
-
-
-
-
-
     }
+
+
+    public void GunChange(int card)
+    {
+
+
+        switch (card) //Update gun stats here
+        {
+            case 0:
+                {
+                    bSty = bulletStyle.normal;
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 20;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 1000;
+                    break;
+                }
+            case 1:
+                {
+                    bSty = bulletStyle.fire;
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 100;
+                    windupReq = 0;
+                    size = 3;
+                    fireForce = 1200;
+                    break;
+                }
+            case 2:
+                {
+                    bSty = bulletStyle.fire;
+                    bulletNumber = 3;
+                    inaccuracy = .5f;
+                    heat = 0;
+                    windupReq = 0;
+                    size = 0.5f;
+                    fireForce = 200;
+                    break;
+                }
+            case 3:
+                {
+                    bSty = bulletStyle.normal;
+                    bulletNumber = 10;
+                    inaccuracy = 0.1f;
+                    heat = 60;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 800;
+                    break;
+                }
+            case 4:
+                {
+                    bSty = bulletStyle.normal;
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 0;
+                    windupReq = 20;
+                    size = 0.5f;
+                    fireForce = 2500;
+                    break;
+                }
+            case 5:
+                {
+                    bSty = bulletStyle.normal;
+                    bulletNumber = 6;
+                    inaccuracy = 0.2f;
+                    heat = 20;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 1000;
+                    break;
+                }
+            case 6:
+                {
+
+                    bSty = bulletStyle.bomb;
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 40;
+                    windupReq = 0;
+                    size = 2;
+                    fireForce = 1000;
+                    break;
+                }
+            case 7:
+                {
+                    bSty = bulletStyle.laser;
+                    bulletNumber = 1;
+                    inaccuracy = 0;
+                    heat = 30;
+                    windupReq = 0;
+                    size = 1;
+                    fireForce = 1000;
+                    break;
+                }
+            case 8:
+                {
+                    bSty = bulletStyle.normal;
+                    bulletNumber = 25;
+                    inaccuracy = 0.25f;
+                    heat = 2;
+                    windupReq = 60;
+                    size = 1;
+                    fireForce = 3000;
+                    break;
+                }
+
+                
+
+        }
+        shot = shots[(int)bSty];
+    }
+
+
+
+}
