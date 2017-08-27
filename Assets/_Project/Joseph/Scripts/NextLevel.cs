49bb49e4ba3e1145fcbab2418b6ffcd3eee8dc87
MkfShard <hartmaniac@gmail.com>
2017-08-27 10:48:08 -0700

    Revert "Revert "Menu -> Intro -> Arena in place""
    This reverts commit 8f11d1615b30f7a3b2e440499c2c5d1fa2f46ad4.

---
+++ Assets/_Project/Joseph/Scripts/NextLevel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextLevel : MonoBehaviour {
+
+    public int next;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    public void Proceed()
+    {
+        SceneManager.LoadScene(next);
+    }
+
+    public void Leave()
+    {
+        Application.Quit();
+    }
+
+}
