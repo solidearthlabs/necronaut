57bd22c19f09fcab0b88eeeab4401ec1a7fe07c7
Marc <marc@solidearthvr.com>
2017-08-27 18:55:40 -0700

    add restart when fall

---
+++ Assets/_Project/Scripts/RestartIfFalling.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartIfFalling : MonoBehaviour {
+    public float minY = -100f;
+    public float checkEverySeconds = 4;
+    private void Start()
+    {
+        StartCoroutine(checkIfFalling());
+    }
+
+    // check every few seconds (more efficient than in Update which would check every frame)
+    IEnumerator checkIfFalling()
+    {
+        while (true)
+        {
+            if (transform.position.y < minY)
+            {
+                //Destroy(this.gameObject);
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+            yield return new WaitForSeconds(checkEverySeconds);
+        }
+    }
+}
